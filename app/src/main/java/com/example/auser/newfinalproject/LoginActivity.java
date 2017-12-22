abb004a7cbcf9818a56e7a77fa975f4833aea89d
556677 <556677@585885.com>
2017-12-22 15:07:40 +0800

    background color

--- app/src/main/java/com/example/auser/newfinalproject/LoginActivity.java
+++ app/src/main/java/com/example/auser/newfinalproject/LoginActivity.java
@@ -2,12 +2,15 @@ package com.example.auser.newfinalproject;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +19,10 @@ public class LoginActivity extends AppCompatActivity {
     EditText username;
     EditText pw;
     Button btn;
-    TextView tv;
+    TextView tv,tipText2,tipText3;
+    private LinearLayout description;
+    private ImageView imageView1;
+
     SharedPreferences settings;
     private final String PREF_NAME = "USERNAME";
     private final String PREF_PASSWORD = "PASSWORD";
@@ -30,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         settings = getSharedPreferences(data,0);
         findViews();
         setListeners();
+        playFrameAnimation();
     }
 
     @Override
@@ -48,11 +55,39 @@ public class LoginActivity extends AppCompatActivity {
         pw = (EditText)findViewById(R.id.etPassword);
         btn = (Button)findViewById(R.id.btnSignIn);
         tv = (TextView)findViewById(R.id.tvRegister);
+        description = (LinearLayout)findViewById(R.id.description);
+
+        tipText2 = (TextView) findViewById(R.id.tipText2);
+        tipText3 = (TextView) findViewById(R.id.tipText3);
+        imageView1=(ImageView)findViewById(R.id.imageView1);
     }
 
     void setListeners() {
         btn.setOnClickListener(listener);
         tv.setOnClickListener(tvListener);
+        description.setOnClickListener(descListener);
+    }
+    int clickCount = 1;
+    private View.OnClickListener descListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            if(clickCount ==1){
+                tipText2.setVisibility(View.VISIBLE);
+                clickCount++;
+            }else if(clickCount ==2){
+                tipText3.setVisibility(View.VISIBLE);
+                clickCount++;
+            }else
+                description.setVisibility(View.GONE);
+        }
+    };
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(outState !=null)
+            outState.putInt("clickCount",clickCount);
     }
 
     View.OnClickListener listener = new View.OnClickListener() {
@@ -75,6 +110,7 @@ public class LoginActivity extends AppCompatActivity {
         }
     };
 
+
     View.OnClickListener tvListener = new View.OnClickListener(){
         @Override
         public void onClick(View view) {
@@ -83,4 +119,31 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(intent);
         }
     };
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            clickCount = savedInstanceState.getInt("clickCount");
+            if (clickCount>=3)
+                description.setVisibility(View.GONE);
+            else if (clickCount==2){
+                description.setVisibility(View.VISIBLE);
+                tipText2.setVisibility(View.VISIBLE);
+                tipText3.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+
+
+
+    }
+
+    void playFrameAnimation(){
+        AnimationDrawable anim1=(AnimationDrawable) imageView1.getBackground();
+        anim1.start();
+
+    }
+
+
 }
